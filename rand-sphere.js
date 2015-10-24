3d712bd3dabcbf33572a2af6cea1f040e5ed2056
kchapelier <kevin.chapelier@gmail.com>
2015-10-24 12:15:55 +0200

    make the rng an optional argument

--- rand-sphere.js
+++ rand-sphere.js
@@ -2,21 +2,24 @@
 
 module.exports = sampleSphere
 
-function sampleSphere(d) {
+var defaultRng = Math.random
+
+function sampleSphere(d, rng) {
+  rng = rng || defaultRng
   var v = new Array(d)
   var d2 = Math.floor(d/2)<<1
   var r2 = 0.0
   for(var i=0; i<d2; i+=2) {
-    var rr = -2.0 * Math.log(Math.random())
+    var rr = -2.0 * Math.log(rng())
     var r =  Math.sqrt(rr)
-    var theta = 2.0 * Math.PI * Math.random()
+    var theta = 2.0 * Math.PI * rng()
     r2 += rr
     v[i] = r * Math.cos(theta)
     v[i+1] = r * Math.sin(theta)
   }
   if(d % 2) {
-    var x = Math.sqrt(-2.0 * Math.log(Math.random())) * 
-            Math.cos(2.0 * Math.PI * Math.random())
+    var x = Math.sqrt(-2.0 * Math.log(rng())) *
+            Math.cos(2.0 * Math.PI * rng())
     v[d-1] = x
     r2 += Math.pow(x, 2)
   }
